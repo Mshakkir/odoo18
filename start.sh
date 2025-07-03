da5025e454b3db29bb900fca9f4305db6f271f69
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-03 14:53:19 +0530

    update

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 
 echo "Starting Odoo on Render using PORT=$PORT"
 
-# Replace http_port with Render's dynamic port
+
 sed -i "s/^http_port *= *.*/http_port = ${PORT}/" odoo.conf
 
-# Only initialize if database is not set up yet
-python3 odoo-bin -c odoo.conf -i base --without-demo=all --stop-after-init
 
-# Now start Odoo normally
+#python3 odoo-bin -c odoo.conf -i base --without-demo=all --stop-after-init
+
+
 python3 odoo-bin -c odoo.conf
