441a9a6ba3f3a6c0c0388f42c1b07f6af7c645ff
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-03 14:19:42 +0530

    update

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "Starting Odoo on Render using PORT=${PORT}"
 sed -i "s/^http_port *= *.*/http_port = ${PORT}/" odoo.conf
 
 # Start Odoo
-python3 odoo-bin -d database18 -i base --without-demo=True
+python3 odoo-bin -c odoo.conf
