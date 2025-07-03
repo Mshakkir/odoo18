a100e13a9a710e37a00d47c50df9993f289b14db
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-03 15:34:28 +0530

    fix: add one-time DB init to start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,21 @@
+#!/bin/bash
 
 echo "Starting Odoo on Render using PORT=$PORT"
 
-
+# Update port
 sed -i "s/^http_port *= *.*/http_port = ${PORT}/" odoo.conf
 
+# Check if DB is empty and run base init
+echo "Checking database status..."
+INIT_DB_FLAG_FILE="/opt/render/project/src/.init_done"
 
-#python3 odoo-bin -c odoo.conf -i base --without-demo=all --stop-after-init
+if [ ! -f "$INIT_DB_FLAG_FILE" ]; then
+  echo "First-time DB init running - installing base..."
+  python3 odoo-bin -c odoo.conf -i base --without-demo=all --stop-after-init
+  touch "$INIT_DB_FLAG_FILE"
+else
+  echo "DB already initialized, skipping -i base"
+fi
 
-
-python3 odoo-bin -c odoo.conf
+# Start Odoo
+exec python3 odoo-bin -c odoo.conf
