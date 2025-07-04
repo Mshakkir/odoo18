78834fdb9987c8f3930ab2c10e83d497275118d8
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-04 16:24:52 +0530

    update manifest

--- custom/college_erp/__manifest__.py
+++ custom/college_erp/__manifest__.py
@@ -1,12 +1,22 @@
+
 {
     'name': "College ERP",
     'version': "18.0.1.1",
-    'summary': """An erp for college education""",
-    'description': """From students admistration to exam, this covers aspects of college administartion""",
-    'author': "shakkir",
+    'summary': "An ERP for college education",
+    'description': """
+College ERP module handles:
+- Student admission
+- Exams
+- Administration
+- Timetables and more
+    """,
+    'author': "Shakkir",
+    'license': "LGPL-3",
     'category': "Education",
-    'sequence':1,
+    'sequence': 1,
     'application': True,
-    'auto_install': True,
-    'installable': True
+    'installable': True,
+    'auto_install': False,
+    'depends': ['base'],
+    'data': [],
 }
