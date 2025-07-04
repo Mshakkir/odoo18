ece53da7547aae52619b49570133c5830160f3ff
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-04 15:56:33 +0530

    update

--- custom/college_erp/__manifest__.py
+++ custom/college_erp/__manifest__.py
@@ -1,3 +1,12 @@
 {
-    'name': "College ERP"
+    'name': "College ERP",
+    'version': "18.0.1.1",
+    'summary': """An erp for college education""",
+    'description': """From students admistration to exam, this covers aspects of college administartion""",
+    'author': "shakkir",
+    'category': "Education",
+    'sequence':1,
+    'application': True,
+    'auto_install': True,
+    'installable': True
 }
