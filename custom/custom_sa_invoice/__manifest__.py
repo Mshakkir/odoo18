85024a0e63b5a0dd3dce6469c84c5502e31e73b0
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-10 16:11:10 +0530

    update invoice

---
+++ custom/custom_sa_invoice/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Saudi Custom Invoice",
+    "version": "1.0",
+    "depends": ["account", "base"],
+    "category": "Accounting",
+    "summary": "Saudi Arabia compliant invoice format ",
+    "data": [
+        "views/invoice_report.xml",
+        "views/external_layout.xml",
+    ],
+    "assets": {},
+    "installable": True,
+    "auto_install": False,
+}
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 
 echo "Starting Odoo on Render using PORT=$PORT"
 
