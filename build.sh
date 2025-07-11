2bc5a1af60a5e77aef3e9e3a12dce647fe89b5b6
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-11 14:36:28 +0530

    updated

--- build.sh
+++ build.sh
@@ -4,3 +4,14 @@
 curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
 apt-get install -y nodejs
 npm install -g rtlcss
+
+# Install required dependencies for wkhtmltopdf
+apt-get update && apt-get install -y \
+  xfonts-75dpi xfonts-base fontconfig libxrender1 libxext6 libjpeg62-turbo wget gnupg
+
+# Download and install Odoo-compatible wkhtmltopdf (patched Qt)
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
+dpkg -i wkhtmltox_0.12.6-1.focal_amd64.deb || apt-get install -f -y
+
+# Cleanup
+rm wkhtmltox_0.12.6-1.focal_amd64.deb
