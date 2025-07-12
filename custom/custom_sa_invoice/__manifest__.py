d3965a29070900588e290501d80d8d0c8a1af4ca
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-12 11:21:52 +0530

    update

--- custom/custom_sa_invoice/__manifest__.py
+++ custom/custom_sa_invoice/__manifest__.py
@@ -5,7 +5,7 @@
     'license': 'LGPL-3',
     'data': [
         'views/custom_invoice_template.xml',
-        'views/report_action.xml',
+        'views/override_invoice_report.xml',
     ],
      'assets': {
         'web.report_assets_common': [
