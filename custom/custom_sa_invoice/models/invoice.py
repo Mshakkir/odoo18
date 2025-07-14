42c5e0df148ea9468e62c05f9db8b8a658107e87
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-14 16:08:02 +0530

    updayte

--- custom/custom_sa_invoice/models/invoice.py
+++ custom/custom_sa_invoice/models/invoice.py
@@ -1,22 +1,22 @@
 from odoo import models, fields
-import base64
-import qrcode
-from io import BytesIO
+# import base64
+# import qrcode
+# from io import BytesIO
 from odoo import api
 
-class AccountMove(models.Model):
-    _inherit = 'account.move'
-
-    zatca_qr_code = fields.Char(compute="_generate_zatca_qr", store=False)
-
-    def _generate_zatca_qr(self):
-        for rec in self:
-            qr_content = f"{rec.company_id.name}|{rec.company_id.vat}|{rec.invoice_date}|{rec.amount_total}|{rec.amount_tax}"
-            qr_img = qrcode.make(qr_content)
-            buffered = BytesIO()
-            qr_img.save(buffered, format="PNG")
-            img_str = base64.b64encode(buffered.getvalue()).decode()
-            rec.zatca_qr_code = img_str
+# class AccountMove(models.Model):
+#     _inherit = 'account.move'
+#
+#     zatca_qr_code = fields.Char(compute="_generate_zatca_qr", store=False)
+#
+#     def _generate_zatca_qr(self):
+#         for rec in self:
+#             qr_content = f"{rec.company_id.name}|{rec.company_id.vat}|{rec.invoice_date}|{rec.amount_total}|{rec.amount_tax}"
+#             qr_img = qrcode.make(qr_content)
+#             buffered = BytesIO()
+#             qr_img.save(buffered, format="PNG")
+#             img_str = base64.b64encode(buffered.getvalue()).decode()
+#             rec.zatca_qr_code = img_str
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
@@ -32,17 +32,17 @@ class AccountMoveLine(models.Model):
     custom_vat_amount = fields.Float(string="VAT", compute='_compute_custom_amount', store=True)
     custom_total_price = fields.Float(string="Total Price", compute='_compute_custom_amount', store=True)
     @api.depends('custom_qty', 'custom_rate', 'custom_discount', 'custom_vat_percent')
-    def _compute_custom_amount(self):
+    def _compute_custom_totals(self):
         for line in self:
-            qty = line.custom_qty or 0
-            rate = line.custom_rate or 0
-            discount = line.custom_discount or 0
-            vat_percent = line.custom_vat_percent or 0
+            qty = line.custom_qty or 0.0
+            rate = line.custom_rate or 0.0
+            discount = line.custom_discount or 0.0
+            vat_pct = line.custom_vat_percent or 0.0
 
-            amount = qty * rate
-            discount_amount = amount * (discount / 100)
-            net_amount = amount - discount_amount
-            vat_amount = net_amount * (vat_percent / 100)
+            base_amount = qty * rate
+            discount_amount = base_amount * (discount / 100.0)
+            net_amount = base_amount - discount_amount
+            vat_amount = net_amount * (vat_pct / 100.0)
             total = net_amount + vat_amount
 
             line.custom_amount = net_amount
