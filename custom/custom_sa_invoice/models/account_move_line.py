8830effbc7624a4997af3f2a33d8bc992552a285
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-15 10:04:55 +0530

    add

--- custom/custom_sa_invoice/models/account_move_line.py
+++ custom/custom_sa_invoice/models/account_move_line.py
@@ -1,22 +1,5 @@
 from odoo import models, fields
-# import base64
-# import qrcode
-# from io import BytesIO
 from odoo import api
-
-# class AccountMove(models.Model):
-#     _inherit = 'account.move'
-#
-#     zatca_qr_code = fields.Char(compute="_generate_zatca_qr", store=False)
-#
-#     def _generate_zatca_qr(self):
-#         for rec in self:
-#             qr_content = f"{rec.company_id.name}|{rec.company_id.vat}|{rec.invoice_date}|{rec.amount_total}|{rec.amount_tax}"
-#             qr_img = qrcode.make(qr_content)
-#             buffered = BytesIO()
-#             qr_img.save(buffered, format="PNG")
-#             img_str = base64.b64encode(buffered.getvalue()).decode()
-#             rec.zatca_qr_code = img_str
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
@@ -27,10 +10,10 @@ class AccountMoveLine(models.Model):
     custom_unit = fields.Char(string="Unit")
     custom_rate = fields.Float(string="S. Rate")
     custom_discount = fields.Float(string="Discount (%)")
-    custom_amount = fields.Float(string="Amount", compute='_compute_custom_amount', store=True)
+    custom_amount = fields.Float(string="Amount", compute='_compute_custom_totals', store=True)
     custom_vat_percent = fields.Float(string="VAT %")
-    custom_vat_amount = fields.Float(string="VAT", compute='_compute_custom_amount', store=True)
-    custom_total_price = fields.Float(string="Total Price", compute='_compute_custom_amount', store=True)
+    custom_vat_amount = fields.Float(string="VAT", compute='_compute_custom_totals', store=True)
+    custom_total_price = fields.Float(string="Total Price", compute='_compute_custom_totals', store=True)
     @api.depends('custom_qty', 'custom_rate', 'custom_discount', 'custom_vat_percent')
     def _compute_custom_totals(self):
         for line in self:
