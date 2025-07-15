31a140875589082adf4e194a6c5a326f1a16bfc2
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-15 11:12:46 +0530

    update

--- custom/custom_sa_invoice/models/account_move.py
+++ custom/custom_sa_invoice/models/account_move.py
@@ -1,26 +1,36 @@
 from odoo import models, fields, api
-from .. import zatca_qr
+import base64
+import qrcode
+from io import BytesIO
 from datetime import datetime
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    zatca_qr_code = fields.Char("ZATCA QR Code", compute="_compute_zatca_qr", store=True)
+    zatca_qr_code = fields.Binary("ZATCA QR", compute="_generate_zatca_qr", store=True)
 
-    @api.depends('invoice_date', 'amount_total', 'amount_tax')
-    def _compute_zatca_qr(self):
-        for move in self:
-            if not move.invoice_date:
-                move.zatca_qr_code = ''
+    @api.depends('amount_total', 'amount_tax', 'invoice_date', 'partner_id', 'company_id')
+    def _generate_zatca_qr(self):
+        for rec in self:
+            if not rec.company_id or not rec.company_id.vat:
+                rec.zatca_qr_code = False
                 continue
 
-            seller_name = move.company_id.name or ''
-            vat_number = move.company_id.vat or ''
-            timestamp = move.invoice_date.strftime('%Y-%m-%dT%H:%M:%SZ')  # ISO 8601
-            total = "%.2f" % move.amount_total
-            vat = "%.2f" % move.amount_tax
+            def _encode_field(tag, value):
+                data = bytes([tag, len(value)]) + value.encode('utf-8')
+                return data
 
-            qr_data = zatca_qr.generate_zatca_qr(
-                seller_name, vat_number, timestamp, total, vat
-            )
-            move.zatca_qr_code = qr_data
+            qr_bytes = b''.join([
+                _encode_field(1, rec.company_id.name or ""),
+                _encode_field(2, rec.company_id.vat or ""),
+                _encode_field(3, rec.invoice_date.strftime('%Y-%m-%d %H:%M:%S') if rec.invoice_date else ""),
+                _encode_field(4, str(rec.amount_total)),
+                _encode_field(5, str(rec.amount_tax))
+            ])
+
+            qr_base64 = base64.b64encode(qr_bytes)
+            qr_img = qrcode.make(qr_base64.decode())
+            buffer = BytesIO()
+            qr_img.save(buffer, format="PNG")
+            qr_data = base64.b64encode(buffer.getvalue())
+            rec.zatca_qr_code = qr_data
