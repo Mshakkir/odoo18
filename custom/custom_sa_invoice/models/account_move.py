b69cffdff9f6771e1c9e5716eb7f778b8e1ddf67
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-19 16:43:36 +0530

    update

--- custom/custom_sa_invoice/models/account_move.py
+++ custom/custom_sa_invoice/models/account_move.py
@@ -7,6 +7,7 @@ from datetime import datetime
 class AccountMove(models.Model):
     _inherit = 'account.move'
     po_number = fields.Char(string='P.O Number')
+    delivery_note = fields.Char(string="Delivery Note")
 
     zatca_qr_code = fields.Binary("ZATCA QR", compute="_generate_zatca_qr", store=True)
 
